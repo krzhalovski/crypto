6f558faf364e197012d1a6f196a60102cf4ba39d
krzhalovski <dkrzhalovski@gmail.com>
2019-01-08 13:27:44 +0100

    finished rsa

--- src/rsa/Main.java
+++ src/rsa/Main.java
@@ -1,9 +1,33 @@
 package rsa;
 
+import java.util.Scanner;
+
 public class Main {
+	
+	private static String bytesToString(byte[] encrypted)
+    {
+        String test = "";
+        for (byte b : encrypted)
+        {
+            test += Byte.toString(b);
+        }
+        return test;
+    }
 
 	public static void main(String[] args) {
-
+		RSA rsa = new RSA();
+		Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the plain text:");
+        String s = sc.nextLine();
+        System.out.println("Encrypting String: " + s);
+        System.out.println("String in Bytes: "
+                + bytesToString(s.getBytes()));
+        byte[] encrypted = rsa.encrypt(s.getBytes());
+        byte[] decrypted = rsa.decrypt(encrypted);
+        System.out.println("Decrypting Bytes: " + bytesToString(decrypted));
+        System.out.println("Decrypted String: " + new String(decrypted));
+        
+        sc.close();
 	}
 
 }
--- src/rsa/RSA.java
+++ src/rsa/RSA.java
@@ -1,5 +1,46 @@
 package rsa;
 
-public class RSA {
+import java.math.BigInteger;
+import java.util.Random;
 
+public class RSA {
+	private BigInteger p;
+	private BigInteger q;
+	private BigInteger N;
+    private BigInteger mp;
+    private BigInteger e;
+    private BigInteger d;
+    private int bitlength = 1024;
+    
+    public RSA()
+    {
+        p = BigInteger.probablePrime(bitlength, new Random());
+        q = BigInteger.probablePrime(bitlength, new Random());
+        N = p.multiply(q);
+        mp = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        mp = mp.divide(p.subtract(BigInteger.ONE).gcd(q.subtract(BigInteger.ONE)));
+        e = BigInteger.probablePrime(bitlength / 2, new Random());
+        while (mp.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(mp) < 0)
+        {
+            e.add(BigInteger.ONE);
+        }
+        d = e.modInverse(mp);
+    }
+ 
+    public RSA(BigInteger e, BigInteger d, BigInteger N)
+    {
+        this.e = e;
+        this.d = d;
+        this.N = N;
+    }
+ 
+    public byte[] encrypt(byte[] message)
+    {
+        return (new BigInteger(message)).modPow(e, N).toByteArray();
+    }
+ 
+    public byte[] decrypt(byte[] message)
+    {
+        return (new BigInteger(message)).modPow(d, N).toByteArray();
+    }
 }
